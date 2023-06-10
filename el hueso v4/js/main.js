59c2d03820497a03887251c2824acd85aa8adfa4
Nico337-ctrl <110873835+Nico337-ctrl@users.noreply.github.com>
2023-06-10 14:09:09 -0500

    Subida de el hueso v4

---
+++ el hueso v4/js/main.js
@@ -0,0 +1,6 @@
+let slider = document.querySelector(".slider-container")
+let sliderindividual = document.querySelectorAll(".contenido")
+let contador = 1;
+let width = sliderindividual(0).clientWidth;
+let intervalo = 3;
+ 
