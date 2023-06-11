01aafb49f01c5aee23d2cd8f0681bfeb1cf0027a
Nico337-ctrl <110873835+Nico337-ctrl@users.noreply.github.com>
2023-06-11 09:36:48 -0500

    agregando efecto smooth scroll

--- el hueso v4/js/main.js
+++ el hueso v4/js/main.js
@@ -1,6 +1,40 @@
-let slider = document.querySelector(".slider-container")
-let sliderindividual = document.querySelectorAll(".contenido")
-let contador = 1;
-let width = sliderindividual(0).clientWidth;
-let intervalo = 3;
- 
+// Función para realizar el smooth scroll
+function smoothScroll(target, duration) {
+  const targetElement = document.querySelector(target);
+  const targetPosition = targetElement.offsetTop;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const scrollAmount = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, scrollAmount);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Función para crear un efecto de aceleración suave en el scroll
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+// Asignar el evento de click a los enlaces o elementos que deseas que activen el smooth scroll
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('a[href^="#"]');
+  links.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const target = link.getAttribute('href');
+      smoothScroll(target, 1000); // Duración del scroll en milisegundos (ej: 1000 = 1 segundo)
+    });
+  });
+});
+
+  
